b516ffb22cab47385c60182e370d3a94ad8d8715
ctiris <48613879+ctiris@users.noreply.github.com>
2022-10-11 18:17:44 -0700

    html files

---
+++ _nuxt/index.47a3b8b8.mjs
@@ -0,0 +1,1 @@
+import{a as e,b as a,o,e as t}from"./entry.98d73bdf.mjs";const p={__name:"index",setup(n){return e(()=>{a({path:"/work",replace:!1})}),(r,s)=>(o(),t("div"))}};export{p as default};
---
+++ _nuxt/trek.2970d042.mjs
@@ -0,0 +1,1 @@
+const r=""+new URL("trek.a0a23598.png",import.meta.url).href;export{r as _};
