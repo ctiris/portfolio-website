b683164f2ce54a93cb4f389b3a17be3439a1007f
ctiris <48613879+ctiris@users.noreply.github.com>
2022-10-31 10:01:06 -0700

    fixed links and styling

---
+++ _nuxt/index.c9003362.mjs
@@ -0,0 +1,1 @@
+import{a as e,b as a,o,e as t}from"./entry.31c808d1.mjs";const p={__name:"index",setup(n){return e(()=>{a({path:"/work",replace:!1})}),(r,s)=>(o(),t("div"))}};export{p as default};
--- _nuxt/trek.2970d042.mjs
+++
@@ -1,1 +0,0 @@
-const r=""+new URL("trek.a0a23598.png",import.meta.url).href;export{r as _};
