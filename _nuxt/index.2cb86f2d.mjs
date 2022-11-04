5921dff26b9299fb24b7acf6fd158e530de64416
ctiris <48613879+ctiris@users.noreply.github.com>
2022-11-04 12:03:04 -0700

    removed blur again

---
+++ _nuxt/index.2cb86f2d.mjs
@@ -0,0 +1,1 @@
+import{a as e,b as a,o,e as t}from"./entry.8d0e5f17.mjs";const p={__name:"index",setup(n){return e(()=>{a({path:"/work",replace:!1})}),(r,s)=>(o(),t("div"))}};export{p as default};
--- _nuxt/index.c9003362.mjs
+++
@@ -1,1 +0,0 @@
-import{a as e,b as a,o,e as t}from"./entry.31c808d1.mjs";const p={__name:"index",setup(n){return e(()=>{a({path:"/work",replace:!1})}),(r,s)=>(o(),t("div"))}};export{p as default};
